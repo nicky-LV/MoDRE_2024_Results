7a74f3868664b51f3adb20520f5468e25f8dcf93
Nicky <69170732+nicky-LV@users.noreply.github.com>
2024-04-24 20:58:23 +0100

    Delete Additional_Resources/Additional_Resources/RDMSim_Python directory

--- Additional_Resources/Additional_Resources/RDMSim_Python/SimpleSimulationExample/SAS/tests.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from .main import POMDP
-
-@pytest.fixture
-def pomdp():
-    thresholds = [0.5, 0.5, 0.5]  # active_links, bandwidth_consumption, write_time
-    return POMDP()
-def test_bellman_equation():
